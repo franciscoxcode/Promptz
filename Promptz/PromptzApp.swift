3a7488f43027036a9fa8debc2a946c3dc961902e
Francisco Casillas <chiccasillas@gmail.com>
2025-10-21 18:25:38 -0600

    Initial Commit

---
+++ Promptz/PromptzApp.swift
@@ -0,0 +1,17 @@
+//
+//  PromptzApp.swift
+//  Promptz
+//
+//  Created by Francisco Jean on 21/10/25.
+//
+
+import SwiftUI
+
+@main
+struct PromptzApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
